29729b370ca946a7959f5c8465258f3f3c3bc620
alresave <alresave@gmail.com>
2026-05-13 11:45:58 -0600

    Add birthday clock page

---
+++ script.js
@@ -0,0 +1,18 @@
+const clock = document.querySelector("#clock");
+
+const formatter = new Intl.DateTimeFormat("en-US", {
+  hour: "numeric",
+  minute: "2-digit",
+  hour12: true,
+});
+
+function updateClock() {
+  const now = new Date();
+  const value = formatter.format(now);
+
+  clock.textContent = value;
+  clock.dateTime = now.toISOString();
+}
+
+updateClock();
+setInterval(updateClock, 1000);
